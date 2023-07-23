e5288d35859bd06f8c5f099709a2664af8399567
Dharmesh-gohil <dharmesh.vatsalgohil@gmail.com>
2023-07-24 02:34:53 +0530

    it s take too much time to complete

---
+++ src/Hero.jsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>Manage Any Content<br />Any where</h1>
+        <p>Strapi is the leading open-source headless CMS.It is 100% JavaScript and fully Customizable</p>
+      </div>
+    </div>
+  )
+}
+export default Hero
